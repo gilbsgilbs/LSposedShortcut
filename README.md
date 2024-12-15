0e4cc1bb1f04e4a79469c10a5e69ceea550e71bd
Gilbert Gilb's <gilbsgilbert@gmail.com>
2024-12-15 15:43:05 +0100

    update intent category

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is just a simple shortcut that lets to start LSposed UI from the launcher.
+This is just a simple shortcut that lets to start LSposed Parasitic Manager
+from the launcher.
--- app/src/main/java/org/gilbsgilbs/lspshrt/MainActivity.kt
+++ app/src/main/java/org/gilbsgilbs/lspshrt/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Runtime.getRuntime().exec("su -c am start-activity -n com.android.shell/.BugreportWarningActivity -c com.google.android.lspmngr.LAUNCH_MANAGER")
+        Runtime.getRuntime().exec("su -c am start-activity -n com.android.shell/.BugreportWarningActivity -c org.lsposed.manager.LAUNCH_MANAGER")
         finish()
     }
 }
